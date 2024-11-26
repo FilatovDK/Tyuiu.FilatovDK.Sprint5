7eef4cd413d9d3b014798ab07d82be1d1297a2fd
kiki769 <fiirve@bk.ru>
2024-11-26 18:10:07 +0500

    Task6

---
+++ Tyuiu.FilatovDK.Sprint5.Task6.V20.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+using Tyuiu.FilatovDK.Sprint5.Task6.V20.Lib;
+namespace Tyuiu.FilatovDK.Sprint5.Task6.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask6V20.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.FilatovDK.Sprint5.Task6.V20/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.FilatovDK.Sprint5.Task6.V20.Lib;
+namespace Tyuiu.FilatovDK.Sprint5.Task6.V20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Спринт #5 | Выполнил: Филатов.Д.К | СМАРТБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Филатов Денис Константинович |  СМАРТБ-24-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask6V20.txt";
+
+
+
+
+
+            Console.WriteLine("данные находятся в файле " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+
+        }
+    }
+}
