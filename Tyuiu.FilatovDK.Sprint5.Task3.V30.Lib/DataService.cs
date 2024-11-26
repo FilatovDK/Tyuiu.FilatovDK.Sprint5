b8a10044ec6595f37c450ac548e0fc9bb01b8270
kiki769 <fiirve@bk.ru>
2024-11-26 17:57:07 +0500

    Task3

---
+++ Tyuiu.FilatovDK.Sprint5.Task3.V30.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.FilatovDK.Sprint5.Task3.V30.Lib
+{
+    public class DataService : ISprint5Task3V30
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+            double y = Math.Round((Math.Pow(x, 3) - 1) / (4 * Math.Pow(x, 2)), 3);
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.FilatovDK.Sprint5.Task3.V30.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.FilatovDK.Sprint5.Task3.V30.Lib;
+namespace Tyuiu.FilatovDK.Sprint5.Task3.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+            FileInfo fileInfo = new FileInfo(path);
+            bool Exists = fileInfo.Exists;
+            Assert.IsTrue(Exists);
+        }
+    }
+}
